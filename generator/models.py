369b1a34a7a2a83ac235b5f1d9c345c6f403ad24
2duckyu <duckyu@ajou.ac.kr>
2021-09-26 23:55:44 +0900

    '수정'

--- generator/models.py
+++ generator/models.py
@@ -7,7 +7,7 @@ class character(db.Model):
     feature = db.Column(db.Text(), nullable=False)
 
 # from generator.models import character
-# q = character(novel='재혼 황후', character='소비에슈 트로비 빅트', gender='M', feature='villain/rich/selfish')
+# q = character(novel='재혼 황후', character='나비에 엘리 트로비', gender='F', feature='coldHearted/rich/generous')
 # from generator import db
 # db.session.add(q)
 # db.session.commit()
